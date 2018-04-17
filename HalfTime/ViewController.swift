be0436e1d5cd5a329da0aaadea151996baecb71b
clokk <connorleisz@gmail.com>
2018-04-17 06:14:11 -0500

    added phone auth

--- HalfTime/ViewController.swift
+++ HalfTime/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
-
+import FirebaseAuth
 class ViewController: UIViewController {
 
+    @IBOutlet weak var phoneNum: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func sendCode(_ sender: Any) {
+        let alert = UIAlertController(title: "phone number", message: "Is this your phone number? \n \(phoneNum.text!)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Yes", style: .default) { (UIAlertAction) in
+            PhoneAuthProvider.provider().verifyPhoneNumber(self.phoneNum.text!) { (verificationID, error) in
+                if error != nil {
+                    print("error: \(String(describing: error?.localizedDescription))")
+                } else {
+                    let defaults = UserDefaults.standard
+                    defaults.set(verificationID, forKey: "authVID")
+                    self.performSegue(withIdentifier: "code", sender: Any?.self)
+                    
+                }
+            }
+        }
+        let cancel = UIAlertAction(title: "No", style: .cancel, handler: nil)
+        alert.addAction(action)
+        alert.addAction(cancel)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
 
 }
 
---
+++ Pods/Target Support Files/Pods-HalfTime/Pods-HalfTime-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HalfTime : NSObject
+@end
+@implementation PodsDummy_Pods_HalfTime
+@end
