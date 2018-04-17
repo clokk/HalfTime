4f55a0f33a2e469f8227015d42722d41c14b68fa
clokk <connorleisz@gmail.com>
2018-04-17 03:38:08 -0500

    Initial Commit

---
+++ HalfTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HalfTime
+//
+//  Created by Connor Leisz on 4/17/18.
+//  Copyright © 2018 Connor Leisz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
